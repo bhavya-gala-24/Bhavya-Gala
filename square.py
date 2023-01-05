6bde2834a4f8503e28b3b987f3804fe4767f76cd
Bhavya Gala <121996336+bhavya-gala-24@users.noreply.github.com>
2023-01-05 09:46:32 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,12 @@
+class square:
+    a=float(input("enter side"))
+    def area(self):
+        print("area of square is",self.a*self.a)
+    def perimeter(self):
+        print("perimeter of square is",4*self.a)
+
+
+
+c=square()
+x=c.area()
+y=c.perimeter()        
